a10f4eba5ad577d4e4936b7780127acf5cf73c50
Kaustubh Karnik <kkarnik5@gmail.com>
2026-05-22 14:57:10 +0530

    update 6

--- README.md
+++
@@ -1,2 +0,0 @@
-# vsm-forms
-
--- supabase/migrations/003_add_ecc_others_team.sql
+++
@@ -1,7 +0,0 @@
--- Update team check constraint to include ECC and Others
-ALTER TABLE forms
-  DROP CONSTRAINT IF EXISTS forms_team_check;
-
-ALTER TABLE forms
-  ADD CONSTRAINT forms_team_check
-  CHECK (team IN ('Yuva Chetana', 'Gram Vikas', 'Nalanda', 'MUSE', 'SDA', 'ECC', 'Others'));
